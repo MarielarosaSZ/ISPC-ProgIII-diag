8f9db2eb8681e9573f7a4bdc2e45b4e24569f9e8
MarielarosaSZ <mariela_suarezz@hotmail.com>
2026-04-04 22:39:16 -0300

    agregado de importaciones necesarias

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from rest_framework import serializers
 from encrypted_model_fields.fields import EncryptedCharField
 
 # Create your models here.
@@ -7,3 +8,23 @@ from encrypted_model_fields.fields import EncryptedCharField
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     encrypted_info = EncryptedCharField(max_length=100)  # Example encrypted field
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,8 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
-from rest_framework import status
+from rest_framework import status 
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer, UserSerializer
 
 class AccountsAPITest(APITestCase):
     def test_register(self):
